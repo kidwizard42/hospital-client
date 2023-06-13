c1215cefbd8f3dc7c6b3626ab072e094cbf17118
Darwood Antoine <darwooda@gmail.com>
2023-06-13 10:32:57 -0400

    files ordered nav issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { EmailComponent } from './email/email.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { EmailComponent } from './components/email/email.component';
 import { RouterModule } from '@angular/router';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, EmailComponent, NavBarComponent],
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    EmailComponent,
+    NavBarComponent,
+  ],
   imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
     BrowserModule,
     RouterModule.forRoot([
       { path: '', component: HomePageComponent, pathMatch: 'full' },
